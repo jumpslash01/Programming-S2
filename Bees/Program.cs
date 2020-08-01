b18678ca9a2a91f3fbf7294c3b0a501cfa2e080d
Zak <6820492@student.swin.edu.au>
2020-08-01 17:52:10 +1000

    comments added

--- Bees/Program.cs
+++ Bees/Program.cs
@@ -4,28 +4,32 @@ namespace Bees {
     class Program {
 
         static void Main (string[] args) {
+
+            //CREATE BEES
             Bees Bee1 = new Bees ("John", 3.2f);
             Bees Bee2 = new Bees ("Paul", 2.7f);
             Bees Bee3 = new Bees ("George", 1.1f);
             Bees Bee4 = new Bees ("Ringo", 2.0f);
-
             Bees Bee5 = new Bees ("Kurt", 2.3f);
             Bees Bee6 = new Bees ("Dave", 7.4f);
             Bees Bee7 = new Bees ("Krist", 1.5f);
 
+            //CREATE HIVES
             hive hive1 = new hive ();
             hive hive2 = new hive ();
-
+            
+            //CALL METHOD TO FILL HIVES
             hive1.FillHive1 (Bee1, Bee2, Bee3, Bee4);
             hive2.FillHive2 (Bee5, Bee6, Bee7);
 
-            //USER INPUTS 
+            //USER INPUTS AMOUNT OF DAYS TO COLLECT HONEY
             System.Console.WriteLine ("Enter the amount of days hive 1 will gather for");
             float days1 = float.Parse (Console.ReadLine ());
 
             System.Console.WriteLine ("Enter the amount of days hive 2 will gather for");
             float days2 = float.Parse (Console.ReadLine ());
 
+            //CALL METHOD TO COLLECT HONEY
             hive1.FlyMyPrettys (days1);
             hive2.FlyMyPrettys2 (days2);
 
--- Bees/hive.cs
+++ Bees/hive.cs
@@ -3,24 +3,24 @@ namespace Bees {
 
         public Bees bee1, bee2, bee3, bee4, bee5, bee6, bee7;
 
-        public int maxbees = 4; 
+        public int maxbees = 4; //This does nothing lol
 
-        public void FillHive1 (Bees newbee1, Bees newbee2, Bees newbee3, Bees newbee4) {
+        public void FillHive1 (Bees newbee1, Bees newbee2, Bees newbee3, Bees newbee4) {    //method to fill hive 1 with bees
             bee1 = newbee1;
             bee2 = newbee2;
             bee3 = newbee3;
             bee4 = newbee4;
         }
-        public void FillHive2 (Bees newbee1, Bees newbee2, Bees newbee3) {
+        public void FillHive2 (Bees newbee1, Bees newbee2, Bees newbee3) {  //fill hiv e2 with bees 
             bee5 = newbee1;
             bee6 = newbee2;
             bee7 = newbee3;
         }
 
-        public void FlyMyPrettys (float day) {
+        public void FlyMyPrettys (float day) {  //sends all hive 1 bees out collect honey based on user input days
             System.Console.WriteLine ((bee1.size + bee2.size + bee3.size + bee4.size) * (0.2f) * day);
         }
-        public void FlyMyPrettys2 (float day) {
+        public void FlyMyPrettys2 (float day) { //sends all hive 2 bees out collect honey based on user input days
             System.Console.WriteLine ((bee5.size + bee6.size + bee7.size) * (0.2f) * day);
 
         }
